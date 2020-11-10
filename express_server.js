5491a1c7f3bfcfb253312d942b12b3a969f59801
Daniel Robertson <danieljrobertson@gmail.com>
2020-11-10 02:06:03 +0000

    linting and comments before i end the day

--- express_server.js
+++ express_server.js
@@ -2,54 +2,53 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080; // default port 8080
-
+// this will become database with SQL access
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
-
-function generateRandomString() {
+// used to generate shortURL;
+const generateRandomString = () => {
   const randomString = Math.random().toString(36).substring(2, 8);
   return randomString;
 };
 
-
+// allows inline JS/html integration
 app.set('view engine', 'ejs');
-
+// parses body of response
 app.use(bodyParser.urlencoded({extended: true}));
-
+// posting logic
 app.post("/urls", (req, res) => {
-  const longURL = req.body.longURL;
-  const shortURL = generateRandomString()
-  urlDatabase[shortURL] = `http://${longURL}`;  // Log the POST request body to the console
-  console.log(urlDatabase);
-  res.redirect(`/urls/${shortURL}`);         // Respond with 'Ok' (we will replace this)
+  const longURL = req.body.longURL; // gets target URL from request body,
+  const shortURL = generateRandomString(); // random string
+  urlDatabase[shortURL] = `http://${longURL}`;  // adds new shortURL : longURL key:value to database object (this will change to SQL)
+  res.redirect(`/urls/${shortURL}`);         // redirects to shortURL instance!
 });
-
+// index of tiny URLS
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
-})
-
+  res.render("urls_index", templateVars);
+});
+// go forth and make a tiny link
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
-
+// why not keep it useful for the APIs
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
+// IMPORTANT: DON'T BUILD any "/urls/... below this!
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { 
-    shortURL: req.params.shortURL, 
-    longURL: urlDatabase[req.params.shortURL] 
+  const templateVars = {
+    shortURL: req.params.shortURL, //this appears in the html
+    longURL: urlDatabase[req.params.shortURL]
   };
   res.render("urls_show", templateVars);
 });
-
+// Redirects shortform URL directly to target webaddress
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
-  const longURL = urlDatabase[shortURL]; 
+  const longURL = urlDatabase[shortURL];
      
   res.redirect(longURL);
     
@@ -57,7 +56,7 @@ app.get("/u/:shortURL", (req, res) => {
 });
 
 
-
+// crank this baby open
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
